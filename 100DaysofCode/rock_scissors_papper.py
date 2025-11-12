b2ee9b3ef1f4c23bcdc30a29d099180d15cd17e9
Radi Slabov <radevbgvn@gmail.com>
2025-11-12 15:25:38 +0200

    rock_scissors_paper

---
+++ 100DaysofCode/rock_scissors_papper.py
@@ -0,0 +1,51 @@
+import random
+
+print("Make your choice by typing rock, paper or scissors followed by enter: \n")
+user_choice = input()
+options = ["rock", "paper", "scissors"]
+computer_choice = random.choice(options)
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+if user_choice == "rock" and computer_choice == "rock":
+    print(rock, rock, "Even, try again!")
+elif user_choice == "rock" and computer_choice == "paper":
+    print(rock, paper, "Paper beats Rock! You lose!")
+elif user_choice == "rock" and computer_choice == "scissors":
+    print(rock, scissors, "Rock beats scissors! You win!")
+elif user_choice == "paper" and computer_choice == "rock":
+    print(paper, rock, "Paper beats rock! You win!")
+elif user_choice == "paper" and computer_choice == "paper":
+    print(paper, paper, "Even, try again!")
+elif user_choice == "paper" and computer_choice == "scissors":
+    print(paper, scissors, "Scissors beat paper! You lose!")
+elif user_choice == "scissors" and computer_choice == "scissors":
+    print(scissors, scissors, "Even, try again!")
+elif user_choice == "scissors" and computer_choice == "paper":
+    print(scissors, paper, "Scissors beat paper! You win!")
+elif user_choice == "scissors" and computer_choice == "rock":
+    print(scissors, rock, "Rock beats scissors! You lose!")
